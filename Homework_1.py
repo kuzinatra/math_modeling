7d23b6cb6f1d5ab7a724a633cef04fa39e5fd6cd
kuzinatra <72644815+kuzinatra@users.noreply.github.com>
2020-10-16 21:27:32 +0200

    Add files via upload
    123

---
+++ Homework_1.py
@@ -0,0 +1,26 @@
+#Задание 1
+print ('Hello, World!')
+
+print ('аша + Петя = Любовь')
+
+x = 3 + 4
+
+print (x)
+
+#Задание 2
+
+x = 'Hello, World'
+
+print (type(x))
+
+x = 3 + 4
+
+print (type(x))
+
+x = 3 / 4
+
+print (type(x))
+
+x = [1, 2, 5, 10, 100]
+
+print (type(x))
