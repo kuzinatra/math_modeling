8287dee9d22129de982ae1617dc42504e248ba8a
kuzinatra <72644815+kuzinatra@users.noreply.github.com>
2020-10-17 13:36:57 +0200

    Add files via upload
    123

---
+++ lec_1_list.py
@@ -0,0 +1,27 @@
+a = [1, 3, 6] #Создание списка
+
+print (a[0]) #Вывод на экран нулевого элемента списка
+
+b = [8, 10, 11]
+
+c = a + b
+
+print (c)
+
+d = []
+
+d.append(a)
+
+print (d)
+
+d.append (b)
+
+print (d)
+
+print (d[0])
+
+print (d[0][0])
+
+d[0] = 'Hello'
+
+print (d)
