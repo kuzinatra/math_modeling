ada1bb5d855dee3f488da5df3e3f0bf3458dd263
kuzinatra <72644815+kuzinatra@users.noreply.github.com>
2020-10-23 19:36:51 +0200

    Add files via upload
    123

---
+++ homework_1_3_4.py
@@ -0,0 +1,23 @@
+#Номер 3
+x = 3
+q = (3%2)
+r = int(16.7*4.32)
+e = 14.5
+w = 31%12
+t = int(x ** 3.4)
+y = (q + r) / (e + w - t)
+print (y)
+
+#Номер 4
+a = [1, 5, 'Good', 'Bad']
+b = [9, 'Blue', 'Red', 11]
+
+print(a[0]+b[2])
+
+print(a[2]+b[2])
+
+print(a[0]*b[0])
+
+print(a[1]**b[3])
+
+print(a+b)
