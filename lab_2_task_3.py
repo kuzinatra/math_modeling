ad2da31606cb25cef7fffc2fa61d1f2f0a143cb9
kuzinatra <72644815+kuzinatra@users.noreply.github.com>
2020-10-24 14:33:21 +0200

    Add files via upload
    123

---
+++ lab_2_task_3.py
@@ -0,0 +1,5 @@
+a = int(input('Введите a'))
+if a % 4 == 0 and a % 400 == 0:
+    print ('Високосный')
+else:
+    print ('Обычный')
---
+++ lab_2_task_4.py
@@ -0,0 +1,6 @@
+a = int(input('Введите a'))
+i = 1
+d = 1
+
+while a > 1:
+    a = a - 1
