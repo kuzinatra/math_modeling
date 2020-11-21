0ae930280123789d49d97fa3b43127b824a3d16d
kuzinatra <72644815+kuzinatra@users.noreply.github.com>
2020-11-21 15:23:49 +0200

    Add files via upload
    123

--- lab_3_task_4.py
+++ lab_3_task_4.py
@@ -1,11 +1,14 @@
-import numpy as nr
+import numpy as np
 from numpy import sin
 N = int(input( ))
 M = int(input( ))
 A = np.zeros((N,M))
 for i in range(N):
   for j in range(M):
-    A[i][j] = sin(N*(i+1) + M*(j+1))
+    if i == 0 and j ==0:
+      A[i][j] = sin(N*(i) + M*(j))
+    else:
+      A[i][j] = sin(N*(i+1) + M*(j+1))
     if A[i][j] < 0:
       A[i][j] = 0
 print(A)
